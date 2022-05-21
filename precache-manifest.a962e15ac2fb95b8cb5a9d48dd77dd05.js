5722866966c6441e673b5ab01264740db91fb6b3
grant2695 <grant.w.tucker@gmail.com>
2022-05-21 18:09:52 -0400

    Updates

---
+++ precache-manifest.a962e15ac2fb95b8cb5a9d48dd77dd05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73812ba296ae45fe3a51a703eb267eb8",
+    "url": "/MY-STOCK-TRADER/index.html"
+  },
+  {
+    "revision": "1aee8c8cf53f8241e216",
+    "url": "/MY-STOCK-TRADER/static/css/main.e833a1b4.chunk.css"
+  },
+  {
+    "revision": "3ae007e296ae319d6e02",
+    "url": "/MY-STOCK-TRADER/static/js/2.29945ac5.chunk.js"
+  },
+  {
+    "revision": "1aee8c8cf53f8241e216",
+    "url": "/MY-STOCK-TRADER/static/js/main.bb86d6f8.chunk.js"
+  },
+  {
+    "revision": "406705b7c49551138adf",
+    "url": "/MY-STOCK-TRADER/static/js/runtime~main.01c2fc00.js"
+  }
+]);
